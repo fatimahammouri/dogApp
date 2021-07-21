b57c9401fb18b09d2126abd9e7890a0c30f3672a
Fatima <fatima.hammouri@gmail.com>
2021-07-21 22:24:01 +0000

    created Homepage component

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 """Server for the Doggy App"""
 
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
---
+++ templates/static/jsx/homepage.jsx
@@ -0,0 +1,9 @@
+function Homepage(){
+    return (
+        <React.Fragment>
+            <h1>Hi, Doggy</h1>
+        </React.Fragment>
+    )
+}
+
+ReactDOM.render(<Homepage />, document.getElementById("root"))
