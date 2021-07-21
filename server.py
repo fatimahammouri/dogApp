3a015cb21ff6a54c3fc248c8c6a0913a833ab44c
Fatima <fatima.hammouri@gmail.com>
2021-07-21 22:59:18 +0000

    created route for requesting Dog breeds list from the DOG API

--- server.py
+++ server.py
@@ -10,6 +10,13 @@ def homepage():
 
     return render_template("root.html")
 
+@app.route("/api/breeds")
+def breed_list():
+    """Get all breeds from the DOG API"""
+
+    url = "https://dog.ceo/api/breeds/list/all"
+    
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,3 +1,5 @@
+"use strict";
+
 function Homepage(){
     return (
         <React.Fragment>
