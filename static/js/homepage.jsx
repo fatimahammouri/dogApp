d674686842fcadbec1dde36ef98b0aa505cf354a
Fatima <fatima.hammouri@gmail.com>
2021-07-22 10:49:16 +0000

    fetch data from server and update breeds list

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -8,6 +8,16 @@ function Homepage(){
     const [breeds, setBreeds] = React.useState([]);
 
 
+    // use Reacts' useEffect hook to fetch data from server route
+    // upon success: parse data as JSON into JS object
+    // update the components' state with data
+    React.useEffect( () => {
+        fetch("/api/breeds")
+        .then((response) => response.json())
+        .then((breedData) =>  setBreeds(breedData))
+        }, []);
+    console.log(breeds)
+
     return (
         <React.Fragment>
             <h1>Hi, Doggy</h1>
