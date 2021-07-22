4020c70521764318c042677cda856078013a4811
Fatima <fatima.hammouri@gmail.com>
2021-07-22 00:06:09 +0000

    request all breeds from API

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 """Server for the Doggy App"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, 
 import requests
 
 app = Flask(__name__)
@@ -15,8 +15,14 @@ def homepage():
 def breed_list():
     """Get all breeds from the DOG API"""
 
+    # url: endpoint from the dog API for getting all breeds list
+    # can be found here: https://dog.ceo/dog-api/documentation/ 
     url = "https://dog.ceo/api/breeds/list/all"
-    
+
+    # get function from requests library allow to make a GET requests
+    response = requests.get(url)
+
+
     
 
 if __name__ == "__main__":
